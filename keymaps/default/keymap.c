0fb7d5d5fb1f2655c235c82e3e658d6b56bcbef2
Catalin Plesu <catalinhimself@gmail.com>
2025-08-29 16:29:24 +0300

    debug

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,8 +1,20 @@
 #include QMK_KEYBOARD_H
+#include "print.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(KC_A, KC_B)
+    [0] = LAYOUT(
+        KC_A,    KC_NO
+    ),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        uprintf("Key pressed: %u at [%u,%u]\n", keycode, record->event.key.row, record->event.key.col);
+    } else {
+        uprintf("Key released: %u at [%u,%u]\n", keycode, record->event.key.row, record->event.key.col);
+    }
+    return true;
+}
 
 void keyboard_post_init_user(void) {
     debug_enable = true;
