98c10d403c7d655f9c3d2119df748739dde411f0
Catalin Plesu <catalinhimself@gmail.com>
2025-08-29 16:15:42 +0300

    adding debugging

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,5 +1,10 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(KC_A)
+    [0] = LAYOUT(KC_A, KC_B)
 };
+
+void keyboard_post_init_user(void) {
+    debug_enable = true;
+    debug_matrix = true;
+}
